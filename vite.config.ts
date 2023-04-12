e43ffd13d307ac934e8db0c0f10e4cba3ffade0f
Cesar Mejia <cmejia@gmail.com>
2023-04-12 15:36:43 +0700

    chore: Setup sentry integration with vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,26 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import { sentryVitePlugin } from '@sentry/vite-plugin';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-	plugins: [sveltekit()],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+const config = defineConfig(({ mode }) => {
+	const isProduction = mode === 'production';
+	const env = loadEnv(mode, process.cwd(), '');
+	return {
+		plugins: [
+			sveltekit(),
+			isProduction
+				? sentryVitePlugin({
+						telemetry: false,
+						org: env.PUBLIC_SENTRY_ORG,
+						project: env.PUBLIC_SENTRY_PROJECT,
+						authToken: env.SENTRY_AUTH_TOKEN,
+						include: './svelte-kit/output'
+				  })
+				: ''
+		],
+		test: {
+			include: ['src/**/*.{test,spec}.{js,ts}']
+		}
+	};
 });
