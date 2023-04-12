3def9fcc2ac458d0d2c3bd0866610a57bc319d04
Cesar Mejia <cmejia@gmail.com>
2023-04-12 16:19:17 +0700

    feat: Add client and server error monitoring buttons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,34 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	const triggerServerError = async () => {
+		try {
+			await fetch('/');
+		} catch (error) {
+			console.log(error);
+		}
+		await fetch('/');
+	};
+</script>
+
+<div class="container">
+	<div>
+		<button
+			on:click={() => {
+				throw new Error('This is a client error');
+			}}>Throw Client Error</button
+		>
+
+		<button on:click={triggerServerError}>Throw Server Error</button>
+	</div>
+</div>
+
+<style>
+	:global(body) {
+		margin: 0;
+	}
+	.container {
+		display: grid;
+		place-items: center;
+		height: 100vh;
+		background-color: darkslategrey;
+	}
+</style>
