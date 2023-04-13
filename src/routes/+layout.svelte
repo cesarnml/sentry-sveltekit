ab71ba62132bbda5faae2f8132308feb7e69360e
Cesar Mejia <cmejia@gmail.com>
2023-04-13 12:21:47 +0700

    feat: Add basic navbar; copilot styles

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<nav>
+	<a href="/">Home</a>
+	<a href="/host/homes">Airbnb your home</a>
+</nav>
+<slot />
+
+<style>
+	nav {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		padding: 1rem;
+		background-color: #fff;
+		box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+	}
+	a {
+		color: #000;
+		text-decoration: none;
+	}
+	a:hover {
+		text-decoration: underline;
+	}
+</style>
